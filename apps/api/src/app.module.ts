86846fec5e467df38433d824bde6b081ae5d5cdc
Yerektus <qop6261@gmail.com>
2026-04-27 11:58:50 +0500

    feat: set up NestJS API structure with modules, controllers, and services

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { MongooseModule } from "@nestjs/mongoose";
+import { PassportModule } from "@nestjs/passport";
+import { AuthModule } from "./auth/auth.module";
+import { GamesModule } from "./games/games.module";
+import { LeaderboardModule } from "./leaderboard/leaderboard.module";
+import { PaymentsModule } from "./payments/payments.module";
+import { RoomsModule } from "./rooms/rooms.module";
+import { UsersModule } from "./users/users.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>("MONGODB_URI"),
+        lazyConnection: true
+      })
+    }),
+    PassportModule.register({
+      defaultStrategy: "jwt"
+    }),
+    JwtModule.register({}),
+    AuthModule,
+    UsersModule,
+    GamesModule,
+    RoomsModule,
+    LeaderboardModule,
+    PaymentsModule
+  ]
+})
+export class AppModule {}
---
+++ apps/api/src/main.ts
@@ -0,0 +1,27 @@
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const webOrigin = configService.get<string>("WEB_ORIGIN") ?? "http://localhost:3000";
+  const port = configService.get<number>("PORT") ?? 3001;
+
+  app.enableCors({
+    origin: webOrigin,
+    credentials: true
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
+
+  await app.listen(port);
+}
+
+void bootstrap();
